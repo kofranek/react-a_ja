4163d4b80629749734925a6cd8f57aca49eca0e2
Hollarka <kofranek@gmail.com>
2022-01-09 17:27:06 +0100

    Props. Аргументы компонента.

--- src/App.js
+++ src/App.js
@@ -2,48 +2,19 @@ import React, {useState} from 'react'
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter'
 import './styles/App.css'
+import PostItem from "./components/PosItem";
 
 function App() {
     const [value, setValue] = useState("Text")
 
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. Javascript</strong>
-                    <div>
-                        Javascript - programovací jazyk
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Odstranit</button>
-                </div>
-            </div>
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. Javascript</strong>
-                    <div>
-                        Javascript - programovací jazyk
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Odstranit</button>
-                </div>
-            </div>
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. Javascript</strong>
-                    <div>
-                        Javascript - programovací jazyk
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Odstranit</button>
-                </div>
-            </div>
+            <PostItem  post={{id:1, title:'Javascript', body:'JavaScript description'}}/>
+            <PostItem  post={{id:2, title:'Python', body:'Python description'}}/>
+            <PostItem  post={{id:3, title:'Fortran', body:'Fortran description'}}/>
+            <PostItem  post={{id:4, title:'C++', body:'C++ description'}}/>
         </div>
     )
-        ;
 }
 
 export default App;
