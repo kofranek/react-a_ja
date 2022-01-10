209fae2aeaeaeae2e1e93c6cb4cc6010fa796fa8
Hollarka <kofranek@gmail.com>
2022-01-10 01:24:59 +0100

    создание нового поста

--- src/App.js
+++ src/App.js
@@ -9,23 +9,33 @@ import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id:1, title:'Javascript', body:'JavaScript description'},
-        {id:2, title:'Javascript A', body:'JavaScript description 1'},
-        {id:3, title:'Javascript B', body:'JavaScript description 2'},
-        {id:4, title:'Javascript C', body:'JavaScript description 3'},
+        {id: 1, title: 'Javascript', body: 'JavaScript description'},
+        {id: 2, title: 'Javascript A', body: 'JavaScript description 1'},
+        {id: 3, title: 'Javascript B', body: 'JavaScript description 2'},
+        {id: 4, title: 'Javascript C', body: 'JavaScript description 3'},
     ])
 
     const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
     const bodyInputRef = useRef()
 
 
-    const addNewPost = (e) =>{
+    const addNewPost = (e) => {
         e.preventDefault()
-        console.log('title=',title)
-        console.log('bodyInputRef=',bodyInputRef)
-        console.log('bodyInputRef.current=',bodyInputRef.current)
-        console.log('bodyInputRef.current.value=',bodyInputRef.current.value)
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        if (newPost.title && newPost.body) {
+            setPosts([...posts, newPost])
+            setTitle('')
+            setBody('')
+        }
+        console.log('newPost=', newPost)
+        console.log('posts=', posts)
+
     }
 
 
@@ -33,25 +43,20 @@ function App() {
         <div className="App">
             {/*řízená komponenta*/}
             <MyInput
-                value = {title}
-                onChange = {e => setTitle(e.target.value)}
+                value={title}
+                onChange={e => setTitle(e.target.value)}
                 type='text'
                 placeholder='název zprávy'
             />
 
-            {/*<input */}
-            {/*    ref={bodyInputRef} */}
-            {/*    type='text'  */}
-            {/*/>*/}
-
-            {/*Neřízená komponenta*/}
             <MyInput
-                ref={bodyInputRef}
+                value={body}
+                onChange={e => setBody(e.target.value)}
                 type='text'
                 placeholder='popis zprávy'
             />
             <MyButton type='submit' onClick={addNewPost}>Vytvořit zprávu</MyButton>
-            <PostList posts={posts} title='Seznam zpráv o JavaScriptu' />
+            <PostList posts={posts} title='Seznam zpráv o JavaScriptu'/>
         </div>
     )
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PostItem from "./PosItem";
 
-const PostList = ({posts, title} ) => {
+const PostList = ({posts, title}) => {
     return (
         <div className="App">
             <h1 style={{textAlign: 'center'}}>
                 {title}
             </h1>
-            {posts.map(post =>
-                <PostItem post={post} key={post.id}/>
+            {posts.map((post, index) =>
+                <PostItem number={index + 1} post={post} key={post.id}/>
             )}
         </div>
     )
